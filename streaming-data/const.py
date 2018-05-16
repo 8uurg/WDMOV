cefe7913473dbc26dcfc7c13baea59e9dd83a20b
Lars Stegman <l.s.stegman@student.tudelft.nl>
2018-05-16 11:18:43 +0200

    Create initial version of streaming data from ndov

---
+++ streaming-data/const.py
@@ -0,0 +1,8 @@
+
+# Seriously, don't put a '/' at the end of the address, or ZeroMQ will not see
+# the port number...
+BISON = "tcp://pubsub.besteffort.ndovloket.nl:7658"
+
+
+class envelopes:
+    RIG_KV6 = b"/RIG/KV6posinfo"
